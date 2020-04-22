62b334163c0cef0c9b08e3f7ed2e683e4550abca
Miguel del Pino <migueldelpino1996@gmail.com>
2020-04-22 15:37:12 +0200

    Comprobacion de que da vueltas completada

--- Assets/Cursor.cs
+++ Assets/Cursor.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 
 
-
 public class Cursor : MonoBehaviour
 {
     public enum Cuadrante
@@ -15,7 +14,7 @@ public class Cursor : MonoBehaviour
 
     List<Cuadrante> cuadrantes;
 
-    private Cuadrante cursorCuadrante = Cuadrante.TopRight;
+    private Cuadrante cursorCuadranteActual = Cuadrante.TopRight;
 
     // Start is called before the first frame update
     void Start()
@@ -26,38 +25,92 @@ public class Cursor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ComprobarCuadrante();
+        cursorCuadranteActual = ComprobarCuadrante();
         ComprobarCirculoHecho();
+        ComprobarCambioDeCuadrante();
+        
+
     }
 
-    private void ComprobarCirculoHecho()
-    {
-        throw new NotImplementedException();
+    private void ComprobarCambioDeCuadrante()
+    {        
+        if (cuadrantes.Count == 0)
+        {
+            cuadrantes.Add(cursorCuadranteActual);
+            return;
+        }
+        if (cursorCuadranteActual != cuadrantes[cuadrantes.Count - 1])
+        {
+            cuadrantes.Add(cursorCuadranteActual);            
+            if (cuadrantes.Count > 4)
+            {
+                cuadrantes.RemoveAt(0);
+            }                      
+        }
     }
 
-    private void ComprobarCuadrante()
+    private bool ComprobarCirculoHecho()
     {
-        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        if(pos.x >= 0 && pos.y >= 0)
+        if(cuadrantes.Count == 4)
         {
-            cursorCuadrante = Cuadrante.TopRight;
-        }
-        else if(pos.x >= 0 && pos.y < 0)
-        {
-            cursorCuadrante = Cuadrante.BottomRight;
-        }
-        else if (pos.x < 0 && pos.y >= 0)
-        {
-            cursorCuadrante = Cuadrante.TopLeft;
+            //si el cuadrante en el que estoy es el mismo que el de hace
+            if (cursorCuadranteActual == cuadrantes[0])
+            {
+                bool hayRepetidos = false;
+                //por cada uno de los cuadrantes disponibles
+                for (int i = 0; i < 4; i++)
+                {
+                    Cuadrante cuadranteAComprobar = (Cuadrante)i;
+                    int numCuadrantes = 0;
+
+                    //compruebo que no haya repetidos
+                    for (int j = 0; j < cuadrantes.Count; j++)
+                    {
+                        if (cuadranteAComprobar == cuadrantes[j])
+                            numCuadrantes++;
+                    }
+                    if (numCuadrantes != 1)
+                    {
+                        hayRepetidos = true;
+                    }
+                }
+                if (!hayRepetidos)
+                {
+                    //Se ha dado una vuelta completa
+                    print("Se ha dado una vuelta completa");
+                    cuadrantes.Clear();
+                    return true;
+                }
+                
+            }
+            return false;
         }
         else
         {
-            cursorCuadrante = Cuadrante.BottomLeft;
+            return false;
         }
+        
+
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private Cuadrante ComprobarCuadrante()
     {
-        
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (pos.x >= 0 && pos.y >= 0)
+        {
+            return Cuadrante.TopRight;
+        }
+        else if (pos.x >= 0 && pos.y < 0)
+        {
+            return Cuadrante.BottomRight;
+        }
+        else if (pos.x < 0 && pos.y >= 0)
+        {
+            return Cuadrante.TopLeft;
+        }
+        else
+        {
+            return Cuadrante.BottomLeft;
+        }
     }
 }
