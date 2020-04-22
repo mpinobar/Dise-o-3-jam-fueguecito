ccb6b54b1168e44a87ca1e8ba84dff8e7025326e
Miguel del Pino <migueldelpino1996@gmail.com>
2020-04-22 14:19:17 +0200

    Proyecto

---
+++ Assets/Cursor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Cursor : MonoBehaviour
+{
+    public enum Cuadrante
+    {
+        TopRight, TopLeft, BottomRight, BottomLeft
+    }
+
+    List<Cuadrante> cuadrantes;
+
+    private Cuadrante cursorCuadrante = Cuadrante.TopRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cuadrantes = new List<Cuadrante>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ComprobarCuadrante();
+        ComprobarCirculoHecho();
+    }
+
+    private void ComprobarCirculoHecho()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void ComprobarCuadrante()
+    {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if(pos.x >= 0 && pos.y >= 0)
+        {
+            cursorCuadrante = Cuadrante.TopRight;
+        }
+        else if(pos.x >= 0 && pos.y < 0)
+        {
+            cursorCuadrante = Cuadrante.BottomRight;
+        }
+        else if (pos.x < 0 && pos.y >= 0)
+        {
+            cursorCuadrante = Cuadrante.TopLeft;
+        }
+        else
+        {
+            cursorCuadrante = Cuadrante.BottomLeft;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
