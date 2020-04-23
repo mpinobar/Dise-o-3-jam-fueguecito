643a70621d3fbd3f0160a2b7bef390e84d6def89
Adayer <andres.perez@live.u-tad.com>
2020-04-23 12:15:45 +0200

    Scripts que sirven para todo

---
+++ Assets/ScriptsQueSirvenParaTodo/AppConstant.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppConstant
+{
+    public static readonly string textoAbrirCajaEsp = "Prueba tu suerte: ";
+    public static readonly string textoAbrirCajaEng = "Try your luck: ";
+    public static readonly string textoCogerCajaEsp = "Coger arma";
+    public static readonly string textoCogerCajaEng = "Grab weapon";
+
+    public static readonly string textoPuertaEsp = "Abir puerta: ";
+    public static readonly string textoPuertaEng = "Open door: ";
+
+    public static readonly string textoPerkEsp = "Compra ";
+    public static readonly string textoPerkEng = "Buy ";
+
+    public static readonly string textoCompraParedEsp = "Compra ";
+    public static readonly string textoCompraParedEng = "Buy ";
+    public static readonly string textoRecargaParedEsp = "Compra ";
+    public static readonly string textoRecargaParedEng = "Buy ";
+
+    public static readonly string textoMejoraEsp = "Mejora ";
+    public static readonly string textoMejoraEng = "Upgrade ";
+
+	public static readonly string textoRondaEsp = "Ronda ";
+	public static readonly string textoRondaEng = "Round ";
+
+	public static readonly string textoJugarEsp = "Jugar ";
+	public static readonly string textoJugarEng = "Play ";
+
+	public static readonly string textoEmpezarEsp = "Pulsa cualquier tecla para continuar ";
+	public static readonly string textoEmpezarEng = "Press any key to continue ";
+
+}
+
+public class AppScenes
+{
+    public static readonly string MENU_SCENE = "Menu";
+    public static readonly string GAME_SCENE = "Level";
+    public static readonly string LOADING_SCENE = "Loading";
+}
+
+public class AppPlayerPrefKeys
+{
+	public static readonly string MUSIC_VOLUME = "MusicVolume";
+	public static readonly string SFX_VOLUME = "MusicVolume";
+}
+public class AppPaths
+{
+	public static readonly string PATH_RESOURCE_SFX = "SFX/";
+	public static readonly string PATH_RESOURCE_MUSIC = "Music/";
+}
+public class AudioName
+{
+	public static readonly string GunShotSound = "gunshot";
+	public static readonly string Explosion = "explosion";
+	public static readonly string ButonClick = "click";
+	public static readonly string Recarga = "reload";
+	public static readonly string Buy = "cash";
+
+	public static readonly string BackgroundMusicSound = "backgroundMusic";
+}
